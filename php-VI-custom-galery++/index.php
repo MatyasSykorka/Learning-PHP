6c58b2f27bf0616fb26b546ca46e9050fd7f9407
MatyasSykorka <sykoramatyas225@student.voskh.cz>
2025-01-09 14:49:17 +0100

    edit-links

--- php-VI-custom-galery++/index.php
+++ php-VI-custom-galery++/index.php
@@ -12,7 +12,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="./styles/style.css">
+        <!-- <link rel="stylesheet" href="./styles/style.css"> -->
         <title>
             Gallery
         </title>
